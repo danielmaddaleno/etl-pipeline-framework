2d0f48e15172f54bece278d225722267ecedf22c
Daniel Maddaleno <danielmaddaleno@users.noreply.github.com>
2021-02-23 22:55:02 -0300

    feat: implement base

---
+++ etl/extractors/base.py
@@ -0,0 +1,9 @@
+"""Base extractor protocol."""
+
+from __future__ import annotations
+from typing import Protocol
+import pandas as pd
+
+
+class Extractor(Protocol):
+    def extract(self) -> pd.DataFrame: ...
