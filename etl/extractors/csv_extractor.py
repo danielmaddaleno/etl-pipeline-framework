f5e85878c2ff5b3b2a6e413b66552c0a3678541e
Daniel Maddaleno <danielmaddaleno@users.noreply.github.com>
2021-03-18 14:34:29 -0300

    feat: implement csv extractor

---
+++ etl/extractors/csv_extractor.py
@@ -0,0 +1,15 @@
+"""CSV file extractor."""
+
+from __future__ import annotations
+
+import pandas as pd
+
+
+class CSVExtractor:
+    def __init__(self, path: str, encoding: str = "utf-8", sep: str = ",", **kwargs):
+        self.path = path
+        self.encoding = encoding
+        self.sep = sep
+
+    def extract(self) -> pd.DataFrame:
+        return pd.read_csv(self.path, encoding=self.encoding, sep=self.sep)
