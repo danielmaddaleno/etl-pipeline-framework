086d1578e8ac776d5e1a2fdef5b7cee356fb55df
Daniel Maddaleno <danielmaddaleno@users.noreply.github.com>
2021-03-23 20:02:48 -0300

    feat: implement postgres

---
+++ etl/extractors/postgres.py
@@ -0,0 +1,19 @@
+"""PostgreSQL extractor (requires psycopg2)."""
+
+from __future__ import annotations
+
+import pandas as pd
+
+
+class PostgresExtractor:
+    def __init__(self, connection_string: str, query: str, **kwargs):
+        self.connection_string = connection_string
+        self.query = query
+
+    def extract(self) -> pd.DataFrame:
+        import psycopg2
+        conn = psycopg2.connect(self.connection_string)
+        try:
+            return pd.read_sql(self.query, conn)
+        finally:
+            conn.close()
