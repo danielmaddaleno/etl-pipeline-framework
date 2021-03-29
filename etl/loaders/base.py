a9f206bafd6a9c028ab7822970fea4c5aa2d21f6
Daniel Maddaleno <danielmaddaleno@users.noreply.github.com>
2021-03-29 17:06:04 -0300

    feat: implement base

---
+++ etl/loaders/base.py
@@ -0,0 +1,9 @@
+"""Base loader protocol."""
+
+from __future__ import annotations
+from typing import Protocol
+import pandas as pd
+
+
+class Loader(Protocol):
+    def load(self, df: pd.DataFrame) -> None: ...
