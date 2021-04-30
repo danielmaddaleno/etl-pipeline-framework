22c2068a9c77b439731c2c95295ad9f2184770bd
Daniel Maddaleno <danielmaddaleno@users.noreply.github.com>
2021-04-30 14:40:57 -0300

    feat: implement csv loader

---
+++ etl/loaders/csv_loader.py
@@ -0,0 +1,14 @@
+"""CSV file loader."""
+
+from __future__ import annotations
+from pathlib import Path
+import pandas as pd
+
+
+class CSVLoader:
+    def __init__(self, path: str, **kwargs):
+        self.path = Path(path)
+
+    def load(self, df: pd.DataFrame) -> None:
+        self.path.parent.mkdir(parents=True, exist_ok=True)
+        df.to_csv(self.path, index=False)
