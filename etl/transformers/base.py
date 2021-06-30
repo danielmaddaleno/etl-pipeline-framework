8c3c1a3dc27f73bad7feff504e0798a7a3ecded6
Daniel Maddaleno <danielmaddaleno@users.noreply.github.com>
2021-06-30 10:39:34 -0300

    feat: implement base

---
+++ etl/transformers/base.py
@@ -0,0 +1,9 @@
+"""Base transformer protocol."""
+
+from __future__ import annotations
+from typing import Protocol
+import pandas as pd
+
+
+class Transformer(Protocol):
+    def transform(self, df: pd.DataFrame) -> pd.DataFrame: ...
