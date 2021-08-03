a9ada6b4fb51717212f4f8bd30fabd53e2fcf49c
Daniel Maddaleno <danielmaddaleno@users.noreply.github.com>
2021-08-03 11:30:39 -0300

    feat: implement filter rows

---
+++ etl/transformers/filter_rows.py
@@ -0,0 +1,13 @@
+"""Filter rows transformer using DataFrame.query()."""
+
+from __future__ import annotations
+import pandas as pd
+
+
+class FilterRowsTransformer:
+    def __init__(self, params: str):
+        # params is a query expression string, e.g. "amount > 0"
+        self.expression = params
+
+    def transform(self, df: pd.DataFrame) -> pd.DataFrame:
+        return df.query(self.expression).reset_index(drop=True)
