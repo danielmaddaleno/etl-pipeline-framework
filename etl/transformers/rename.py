633ff8fe9ec55acd5b541e9b5d6900187249cf0c
Daniel Maddaleno <danielmaddaleno@users.noreply.github.com>
2021-08-03 13:50:36 -0300

    feat: implement rename

---
+++ etl/transformers/rename.py
@@ -0,0 +1,13 @@
+"""Rename columns transformer."""
+
+from __future__ import annotations
+import pandas as pd
+
+
+class RenameTransformer:
+    def __init__(self, params: dict):
+        # params = {"old_col": "new_name", ...}
+        self.mapping = params
+
+    def transform(self, df: pd.DataFrame) -> pd.DataFrame:
+        return df.rename(columns=self.mapping)
